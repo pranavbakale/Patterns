cbb2102f623b8dfef83a53392bdf0cb84e1c256c
Pranav Bakale <78615142+pranavbakale@users.noreply.github.com>
2023-02-03 10:05:09 +0530

    Create Pattern 6.cpp

---
+++ Pattern 6.cpp
@@ -0,0 +1,30 @@
+/* 
+Pattern Six
+ 1 2 3
+ 1 2
+ 1
+*/
+
+//Code
+
+#include<iostream>
+using namespace std;
+
+void pattern6(int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i+1;j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    pattern6(n);
+}
